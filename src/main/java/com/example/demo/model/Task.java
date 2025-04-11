aae488ba4e5e71245094b57e290e105fa23f3f2e
vamsee9201 <vamseekrishna9201@gmail.com>
2025-04-10 23:48:10 -0500

    need to finish the to do entity

--- src/main/java/com/example/demo/model/Task.java
+++ src/main/java/com/example/demo/model/Task.java
@@ -12,4 +12,46 @@ public class Task {
 
     private String title;
     private boolean completed;
+
+    public Task() {
+
+    }
+
+    public Task(String title, boolean completed) {
+        this.title = title;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", completed=" + completed +
+                '}';
+    }
 }
