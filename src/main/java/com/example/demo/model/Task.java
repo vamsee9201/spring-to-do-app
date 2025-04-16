7eb7a57927562a64502161ba1d800342d2c17373
vamsee9201 <vamseekrishna9201@gmail.com>
2025-04-15 23:19:57 -0500

    added model and repository

--- src/main/java/com/example/demo/model/Task.java
+++ src/main/java/com/example/demo/model/Task.java
@@ -1,5 +1,6 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,46 +13,4 @@ public class Task {
 
     private String title;
     private boolean completed;
-
-    public Task() {
-
-    }
-
-    public Task(String title, boolean completed) {
-        this.title = title;
-        this.completed = completed;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public boolean isCompleted() {
-        return completed;
-    }
-
-    public void setCompleted(boolean completed) {
-        this.completed = completed;
-    }
-
-    @Override
-    public String toString() {
-        return "Task{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", completed=" + completed +
-                '}';
-    }
 }
